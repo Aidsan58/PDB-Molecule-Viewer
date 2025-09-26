ac4731adbd06c4d56a0bc80acd645e16e3ebf52c
Aidan Rosen <aidanr1@comcast.net>
2025-09-25 21:30:37 -0700

    Added CMakeLists.txt

---
+++ build/CMakeFiles/my_opengl_app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aidan/Code/Projects/opengl-project/src/main.cpp" "CMakeFiles/my_opengl_app.dir/src/main.cpp.o" "gcc" "CMakeFiles/my_opengl_app.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/aidan/Code/Projects/opengl-project/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/aidan/Code/Projects/opengl-project/build/glfw-build/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
