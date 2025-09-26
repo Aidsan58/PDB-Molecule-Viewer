51d478ddcba0192fdd07a7db7d827b0c24210838
Aidan Rosen <aidanr1@comcast.net>
2025-09-26 10:17:20 -0700

    Added boilerplate for window object

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,64 @@
+
 #define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
-#include <glad/gl.h>
+#include <stdio.h>
+#include <iostream>
 
-if (!glfwInit()) {
-    // Initialization failed
+// Viewport changes if user changes window size
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
 
-void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
+// This input function organizes all user input in one place
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
-glfwSetErrorCallback(error_callback);
+int main() {
+    // Instantiate GLFW window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-if (!window) {
-    // Window or OpenGL context creation failed
+    // Create window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);  
+    
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    } 
+
+    glViewport(0, 0, 800, 600);
+
+    // Set viewport resize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render loop
+    while(!glfwWindowShouldClose(window)) {
+        // Clear the screen with a color (optional)
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Place all rendering commands in the render loop
+        processInput(window); // Checks for input every iteration of the render loop (every frame)
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+    return 0;
 }
 
-glfwMakeContextCurrent(window);
-
-glfwTerminate(); // Destroys any remaining windows and resources allocated by glfw
