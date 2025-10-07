b87019cf0ee2abc2b06d2ee0c4151c17a35f8762
Aidan Rosen <aidanr1@comcast.net>
2025-10-06 19:58:19 -0700

    Light source now rotates around to show off Phong's lighting model

--- src/main.cpp
+++ src/main.cpp
@@ -165,6 +165,10 @@ float vertices[] = {
         // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
+        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
